bb1d7665ef32070e933cbc9937be33ef211e5b9e
Sumathi_A <sumathi.a1606@gmail.com>
2026-06-02 11:39:56 +0530

    Day 1 - Strings - AnagramCheck - using Arrays.sort()

---
+++ Strings/AnagramCheck.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+public class AnagramCheck {
+    public static void main(String[] args) {
+        String s1 = "listen";
+        String s2 = "silent";
+        char[] ch1 = s1.toCharArray();
+        char[] ch2 = s2.toCharArray();
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+        if(Arrays.equals(ch1, ch2))
+        {
+            System.out.println("The strings are anagrams.");
+        }
+        else
+        {
+            System.out.println("The strings are not anagrams.");
+        }
+
+    }
+}
