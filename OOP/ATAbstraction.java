7445d31bf7572ba9e2b3239baedfb9fd3035c8ab
Sumathi_A <sumathi.a1606@gmail.com>
2026-06-12 12:02:05 +0530

    Day 2 - Abstraction - ActiveTraders - Report Generation

--- OOP/ATAbstraction.java
+++ OOP/ATAbstraction.java
@@ -1,6 +1,25 @@
 package OOP;
 
-public class ATAbstraction {
+abstract  class ATAbstraction {
+    abstract void generateReport();
+
+    public void showReport() {
+        System.out.println("Report is being generated...");
+       
+    }
+    static class ExtendReport extends ATAbstraction{
+        @Override
+        void generateReport() {
+            System.out.println("Extend Report Generated successfully..!");
+        }
+    }
+public static void main(String[] args) {
+    ATAbstraction report = new ExtendReport();
+   
+    report.showReport();
+    report.generateReport();
     
-    
-}
+} }
+
+//hiding implemenation details and showing only what must be done
+//Parent child relationship - create object parent(left, ref) = child(object type, right) 
